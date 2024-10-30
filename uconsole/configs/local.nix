6691cc402da71a8c892780aa7e13c2e8c6a88498
robert jakub <rj@project2.pl>
2024-10-30 08:13:25 +0100

    remove private cache

--- uconsole/configs/local.nix
+++ uconsole/configs/local.nix
@@ -1,31 +1,20 @@
 {pkgs, ...}: {
-  nix.distributedBuilds = true;
-  nix.buildMachines = [
-    {
-      hostName = "red";
-      systems = ["aarch64-linux"];
-      maxJobs = 24;
-      speedFactor = 30;
-      supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-      protocol = "ssh-ng";
-    }
-  ];
+  nix.distributedBuilds = false;
   nix.settings = {
-    substituters = ["https://cache-nix.project2.xyz/uconsole" "https://nixcache.trustno1.corp/"];
+    substituters = ["https://cache-nix.project2.xyz/uconsole"];
     trusted-substituters = ["https://cache-nix.project2.xyz/uconsole"];
     trusted-public-keys = ["uconsole:t2pv3NWEtXCYY0fgv9BB8r9tRdK+Tz7HYhGq9bXIIck="];
     experimental-features = ["nix-command" "flakes"];
   };
-
   services.openssh.enable = true;
-  users.users.oom = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-  };
   environment.systemPackages = [
     pkgs.vim
     pkgs.alejandra
     pkgs.mc
   ];
   boot.supportedFilesystems.zfs = false;
+  users.users.oom = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+  };
 }
